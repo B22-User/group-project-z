29e97feac5e4d3233d2c46261e601baf87ea6d42
LeylaC <82922377+KingaCeliker@users.noreply.github.com>
2021-07-07 20:41:28 -0400

    more (#7)
    * added main method
    
    * added more
    
    * added more

---
+++ src/test/java/Kinga/State/State.java
@@ -0,0 +1,9 @@
+package Kinga.State;
+
+public class State {
+    public static void main(String[] args) {
+        String state = "headache";
+        System.out.println("This push and pull gave me "+ state);
+//more
+    }
+}
