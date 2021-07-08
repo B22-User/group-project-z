eb53e996ccd42d8ea96b5dcef1571536274b6279
John Clancy <johnpclancy@hotmail.com>
2021-07-07 20:17:51 -0400

    new code 3

--- src/test/java/JohnC/TC100.java
+++ src/test/java/JohnC/TC100.java
@@ -4,5 +4,6 @@ public class TC100 {
     public static void main(String[] args) {
         System.out.println("more cucumbers");
         System.out.println(" cucumbers everywhere");
+        System.out.println(" cucumbers got eaten");
     }
 }
